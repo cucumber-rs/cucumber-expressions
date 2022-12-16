61001161d8185aea22d9d71601a398728e49ba7e
tyranron <tyranron@gmail.com>
2022-12-16 12:52:22 +0100

    Tune lints for 1.66 Rust

--- src/lib.rs
+++ src/lib.rs
@@ -26,6 +26,7 @@
 #![forbid(non_ascii_idents, unsafe_code)]
 #![warn(
     clippy::as_conversions,
+    clippy::as_ptr_cast_mut,
     clippy::assertions_on_result_states,
     clippy::branches_sharing_code,
     clippy::clone_on_ref_ptr,
@@ -34,6 +35,7 @@
     clippy::debug_assert_with_mut_call,
     clippy::decimal_literal_representation,
     clippy::default_union_representation,
+    clippy::derive_partial_eq_without_eq,
     clippy::else_if_without_else,
     clippy::empty_drop,
     clippy::empty_line_after_outer_attr,
@@ -66,6 +68,7 @@
     clippy::nonstandard_macro_braces,
     clippy::option_if_let_else,
     clippy::panic_in_result_fn,
+    clippy::partial_pub_fields,
     clippy::pedantic,
     clippy::print_stderr,
     clippy::print_stdout,
@@ -118,6 +121,8 @@
     unused_tuple_struct_fields,
     variant_size_differences
 )]
+// TODO: Remove on next `derive_more` major version.
+#![allow(clippy::uninlined_format_args)]
 
 pub mod ast;
 mod combinator;

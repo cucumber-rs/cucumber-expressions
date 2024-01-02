6d3ea006848c24b653a73cc1f78bde31a7b0384a
tyranron <tyranron@gmail.com>
2024-01-02 14:12:20 +0100

    Tune lints for 1.75 Rust

--- src/lib.rs
+++ src/lib.rs
@@ -88,6 +88,7 @@
     clippy::pub_without_shorthand,
     clippy::rc_buffer,
     clippy::rc_mutex,
+    clippy::read_zero_byte_vec,
     clippy::readonly_write_lock,
     clippy::redundant_clone,
     clippy::redundant_type_annotations,
